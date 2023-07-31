f79a1a773e0e811fb012929ba4ebc05b63b1fac1
zikowashere <zakdaouma@gmail.com>
2023-07-31 12:26:29 +0100

    change redirectUri And After Authentification

--- global.ts
+++ global.ts
@@ -1,8 +1,18 @@
 import { useState } from "react";
 
 export const accessToken = localStorage.getItem("access_token");
-export const redirectUri = window.location.origin + "/callback/";
-export const redirectAfterAuthtentication = window.location.origin;
+let redirectUri: string;
+let redirectAfterAuthtentication: string;
+// Vérifie si vous êtes dans un environnement de navigateur (client-side)
+if (typeof window !== "undefined") {
+  redirectUri = window.location.origin + "/callback/";
+  redirectAfterAuthtentication = window.location.origin;
+} else {
+  // Si vous êtes dans un environnement de serveur (server-side), définissez une valeur par défaut pour redirectUri
+  redirectUri = "https://spootiffy.netlify.app/callback/";
+  redirectAfterAuthtentication = "https://spootiffy.netlify.app/";
+}
+export { redirectUri, redirectAfterAuthtentication };
 
 export const useCodeVerifer = () => {
   const [codeVerifer, setCodeVerifer] = useState<string>("");
