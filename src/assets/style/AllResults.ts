6a284bde403d2a3a12591c75ed231ff4d0f18b2d
zikowashere <zakdaouma@gmail.com>
2023-08-01 12:46:04 +0100

    modification root grid of All results

---
+++ src/assets/style/AllResults.ts
@@ -0,0 +1,29 @@
+import { CSSProperties } from "react";
+
+export const styleRootFirstDiv: CSSProperties = {
+  display: "flex",
+  justifyItems: "center",
+  height: "calc(100vh - 10%)",
+  overflowY: "auto",
+  width: "100%",
+  marginLeft: "2%",
+};
+export const styleGridOne: CSSProperties = {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr ",
+  gridGap: "5%",
+};
+export const styleGridTwo: CSSProperties = {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr ",
+  gridGap: "5%",
+};
+export const styleGridThree: CSSProperties = {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr ",
+  gridGap: "5%",
+};
+export const styleFirstDiv: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+};
